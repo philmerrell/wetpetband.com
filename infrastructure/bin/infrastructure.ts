cee66e419b0f26b43b4839b74face0ce35114b36
Phil Merrell <philmerrell@boisestate.edu>
2022-02-05 21:16:20 -0700

    add infrastructure file

---
+++ infrastructure/bin/infrastructure.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { ClientStack } from '../lib/infrastructure-stack';
+
+const app = new cdk.App();
+new ClientStack(app, 'ClientStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
