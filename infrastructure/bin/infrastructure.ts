fbcc5f366dc569d8c8656df9d1acf8e02734caf9
Phil Merrell <philmerrell@boisestate.edu>
2022-02-05 21:38:53 -0700

    update stack name

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,6 +4,6 @@ import * as cdk from 'aws-cdk-lib';
 import { ClientStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
-new ClientStack(app, 'ClientStack', {
+new ClientStack(app, 'WetPetClientStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
